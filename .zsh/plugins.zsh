86a268f5fb1593b9cab64536537fbc521dcfd715
Elias Brange <elias.brange@gmail.com>
2016-11-23 20:41:19 +0100

    Moved zsh-syntax-highlighting to oh-my-zsh

--- .zsh/plugins.zsh
+++ .zsh/plugins.zsh
@@ -3,9 +3,6 @@ PLUGINS_FOLDER=$HOME/.zsh/plugins
 # z.sh
 . $PLUGINS_FOLDER/z.sh
 
-# zsh syntax highlighting
-. $PLUGINS_FOLDER/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 # tmux
 . $PLUGINS_FOLDER/custom_tmux.zsh
 
