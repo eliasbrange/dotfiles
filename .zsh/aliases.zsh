f9bdb802b01250afaf54b31aae5b8d4b2ec88426
Elias Brange <elias.brange@gmail.com>
2017-05-03 10:14:21 +0200

    Modified some stuff for linux

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,12 +1,8 @@
 # zsh stuff
 alias zshconf="subl ~/.zshrc ~/.zsh/"
 alias zshrl="source ~/.zshrc"
-alias open='cygstart'
-alias sudo='cygstart --action=runas'
 alias killall='taskkill /F /T /IM'
 alias kp="killall python.exe"
-
-alias subl="/cygdrive/c/Program\ Files/Sublime\ Text\ 3/subl $@"
 
 # Greps
 alias grep='grep --color'
@@ -25,17 +21,12 @@ alias fd='find . -type d -name'
 alias ff='find . -type f -name'
 
 # Dev stuff
-alias setuptng="pushd ~/src/limetng && winpty cmd /c setup.bat; popd"
-alias lisa='winpty.exe /cygdrive/c/src/limeworld/serveradmin/Server/Lundalogik.Lisa.Service.ConsoleHost/bin/Debug/LisaConsole.exe'
-alias wpy='winpty.exe python'
-alias ipy='winpty.exe ipython'
-alias wman='wpy manage.py'
-alias mkvenv='deactivate; rm -r venv; wpy -m virtualenv venv; av'
+alias mkvenv='deactivate; rm -r venv; py -m virtualenv venv; av'
 alias lf='winpty.exe limefu'
 alias pipinst="pip install -r requirements.txt"
 alias pipe="pip install -e"
 alias pipu="pip uninstall -y"
-alias piplic="python 'C:\src\license.py'"
+alias piplic="python 'C:\src\license.py"
 
 # Tell tmux to always expect 256 colors
 alias tmux="tmux -2"
